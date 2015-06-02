21361d488140fc989b7d95998edfce7e05a677a6
skydev <iqpirat@gmail.com>
2015-06-02 09:22:42 +0300

    Azure Support 2

---
+++ AzureBlob/AzureSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureBlob
+{
+    public class AzureSettings
+    {
+        public String ContainerName { get; set; }
+        public String Url { get; set; }
+        public String AccountName { get; set; }
+        public String AccountKey { get; set; }
+
+        public AzureSettings()
+        {
+            ContainerName = "Name";
+            Url = "http://";
+            AccountName = "user";
+            AccountKey = "key";
+        }
+    }
+}
